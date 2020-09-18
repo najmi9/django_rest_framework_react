9536fc56d5c51af6e140269dd3afe289be17d3e4
“Imad <imadnajmi9@gmail.com>
2020-09-18 20:14:38 +0100

    final commit

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
